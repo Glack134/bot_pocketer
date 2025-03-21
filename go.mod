997a4c73d226611cac0fb6b2ec069c47d9b4d115
Midiy <polyk6859@gmail.com>
2025-03-21 22:56:22 +0300

    second commit

--- go.mod
+++ go.mod
@@ -57,7 +57,8 @@ require (
 	go.opentelemetry.io/otel/metric v1.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,1 +1,51 @@
 package server
+
+import (
+	"net/http"
+	"tg_bot/pkg/storage"
+
+	"go.uber.org/zap"
+)
+
+type AuthServer struct {
+	server *http.Server
+	logger *zap.Logger
+
+	storage storage.TokenStorage
+	client  *pocket.Client
+
+	redirectUrl string
+}
+
+func NewAuthServer(redirectUrl string, storage storage.TokenStorage, client *pocket.Client) *AuthServer {
+	return &AuthServer{
+		redirectUrl: redirectUrl,
+		storage:     storage,
+		client:      client,
+	}
+}
+
+func (s *AuthServer) Start() error {
+	s.server = &http.Server{
+		Handler: s,
+		Addr:    ":80",
+	}
+
+	logger, _ := zap.NewDevelopment(zap.Fields(
+		zap.String("app", "authorization server")))
+	defer logger.Sync()
+
+	s.logger = logger
+
+	return s.server.ListenAndServe()
+}
+
+func (s *AuthServer) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		s.logger.Debug("received unavailable HTTP method request",
+			zap.String("method", r.Method))
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	//next
+}
