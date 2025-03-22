334f80ceb73225725389cada44fd2fcf71cd8960
Midiy <polyk6859@gmail.com>
2025-03-22 10:48:32 +0300

    update struct progect

---
+++ pkg/telegram/error.go
@@ -0,0 +1,24 @@
+package telegram
+
+import "errors"
+
+var (
+	invalidUrlError   = errors.New("url is valid")
+	unableToSaveError = errors.New("unable to save link to Pocket")
+)
+
+func (b *Bot) handleError(chatID int64, err error) {
+	var messageText string
+
+	switch err {
+	case invalidUrlError:
+		messageText = b.messages.Errors.InvalidURL
+	case unableToSaveError:
+		messageText = b.messages.Errors.UnableToSave
+	default:
+		messageText = b.messages.Errors.Default
+	}
+
+	msg := tgbotapi.NewMessage(chatID, messageText)
+	b.bot.Send(msg)
+}
